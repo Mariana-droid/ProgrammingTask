e9e8762af8860f3a9c3053fc388c4699ba6b0112
Mariana Charneca <mariana.16.charneca@gmail.com>
2024-03-04 16:38:58 +0000

    Edited Scene debug added

--- Assets/Scripts/debug.cs
+++ Assets/Scripts/debug.cs
@@ -1,14 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class debug : MonoBehaviour
 {
     public InventoryManager inventoryManager;
-    public Item item;
-    public int quantity;
-    public void AddToInventory()
+    public Item[] item;
+    public int[] quantity;
+    public void AddHair()
     {
-        inventoryManager.AddItem(item, quantity);
+        inventoryManager.AddItem(item[0], quantity[0]);
+    }
+    public void AddOutfit1()
+    {
+        inventoryManager.AddItem(item[1], quantity[1]);
+    }
+    public void AddOutfit2()
+    {
+        inventoryManager.AddItem(item[2], quantity[2]);
+    }
+    public void ReloadScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+    public void QuitGame()
+    {
+        // Quit the application
+        Application.Quit();
     }
 }
